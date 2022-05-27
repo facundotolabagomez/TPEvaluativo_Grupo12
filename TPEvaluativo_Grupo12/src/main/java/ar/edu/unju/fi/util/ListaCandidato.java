b40fb9170b1a9aea3734af3c0381196cf78b50f6
Facu TG <Facu TG@DESKTOP-SAMSUNG>
2022-05-27 17:00:44 -0300

    Agregue nuevos candidatos - Obtener edad (clase usuario)

--- TPEvaluativo_Grupo12/src/main/java/ar/edu/unju/fi/util/ListaCandidato.java
+++ TPEvaluativo_Grupo12/src/main/java/ar/edu/unju/fi/util/ListaCandidato.java
@@ -10,7 +10,11 @@ private ArrayList<Candidato> candidatos;
 	
 	public ListaCandidato() {
 		candidatos = new ArrayList<Candidato>();
-		candidatos.add(new Candidato(1,"Queen","Rock","Banda Emblematica de Rock",0));		
+		candidatos.add(new Candidato(1,"Queen","Rock","Banda Emblematica de Rock",0));
+		candidatos.add(new Candidato(2,"ACDC","Rock","Banda Popular de Rock",0));
+		candidatos.add(new Candidato(3,"Guns n Roses","Rock","Banda Problematica de Rock",0));
+		candidatos.add(new Candidato(4,"La Mona","Cuarteto","Icono del cuarteto",0));
+		candidatos.add(new Candidato(5,"Ninguno","-","No me gusta ninguno",0));
 	}
 
 	public ArrayList<Candidato> getCandidatos() {
