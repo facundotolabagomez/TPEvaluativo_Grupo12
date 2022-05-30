dcf750984240fde24009e03c05e30ea06e987abd
Elio <Elio@elio>
2022-05-29 22:08:27 -0300

    templates:gracias,nouser,limite,modif:controller votacion

--- TPEvaluativo_Grupo12/src/main/java/ar/edu/unju/fi/controller/VotacionController.java
+++ TPEvaluativo_Grupo12/src/main/java/ar/edu/unju/fi/controller/VotacionController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import ar.edu.unju.fi.model.Candidato;
+import ar.edu.unju.fi.model.Usuario;
 import ar.edu.unju.fi.util.ListaCandidato;
 
 @Controller
@@ -47,5 +48,37 @@ public class VotacionController {
 		return "redirect:/votacion/gracias";
 	}
 	
+	@GetMapping("/gracias")
+	public String getGraciasPage(Model model) {		
+		return "gracias";
+	}
 	
+	@GetMapping("/fail")
+	public String getLimitePage(Model model) {		
+		return "limite";
+	}
+	
+	@GetMapping("/noexiste")
+	public String getNouserPage(Model model) {		
+		return "nouser";
+	}
+	
+	@GetMapping("/mostrar")
+	public String getMostrarPage(Model model) {
+		int totalvotos=0;
+		double porcentaje=0;
+		for(Candidato candid : listacandidatos.getCandidatos()) {
+			totalvotos=totalvotos+candid.getCantVotos();
+		}
+		for(int i=0;i<listacandidatos.getCandidatos().size();i++) {
+			int votoindividual = listacandidatos.getCandidatos().get(i).getCantVotos();
+			porcentaje=((100.0*votoindividual)/totalvotos);
+			listacandidatos.getCandidatos().get(i).setPorcentaje(porcentaje);
+			LOGGER.info("CANDIDATO-> "+listacandidatos.getCandidatos().get(i).getNombre()+" PORCENTAJE-> "+listacandidatos.getCandidatos().get(i).getPorcentaje());
+		}
+		model.addAttribute("candidato", listacandidatos.getCandidatos());
+		return "mostrar_votacion";
+	}
 }
+	
+
